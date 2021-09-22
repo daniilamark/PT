6c7e72514fd191ba959b088b7a5bbbc83dd8203d
daniilamark <daniilamars@gmail.com>
2021-09-22 11:53:24 +0500

    lab_2 methods - work in VUZ + 7 task

---
+++ src/com/daniilamark/university/lab_2/ProgramTask7.java
@@ -0,0 +1,29 @@
+package com.daniilamark.university.lab_2;
+
+public class ProgramTask7 {
+    public static void main(String[] args) {
+        System.out.println(dayLine(7));
+        System.out.println(dayLine(13));
+        System.out.println(dayLine(18));
+        System.out.println(dayLine(2));
+        System.out.println(" ");
+        System.out.println(dayLine(27));
+        System.out.println(dayLine(-3));
+        System.out.println(dayLine(8));
+        System.out.println(dayLine(4));
+        System.out.println(dayLine(18));
+    }
+
+    static String dayLine(int hour){
+        if (hour > 24 || hour < 0)
+            return "Invalid data";
+        else if (hour > 21 || hour < 6)
+            return "Good night";
+        else if (hour >= 15)
+            return "Good evening";
+        else if (hour >= 11)
+            return "Good after moon";
+        else
+            return "Good morning";
+    }
+}
