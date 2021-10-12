260255ca0c5754cbb86d412627e2193e9686aae3
daniilamark <daniilamars@gmail.com>
2021-10-12 21:53:41 +0500

    lab_2 methods - individual task - v2 work in Home

--- src/com/daniilamark/university/lab_2/ind_task/MyClass.java
+++ src/com/daniilamark/university/lab_2/ind_task/MyClass.java
@@ -5,40 +5,72 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
-public   class MyClass {
-
-    static float arrFloat[] = {12,32,2434,35,24,21,34,221};
-
-
-
+class MyClass {
+    static double arrFloat[] = {12.0, 32.3, 2434.22, 2121.3, 1.65, 32.2};
+    static int arrInt[] = {-12, -32, -24, 21, 1, 5};
 
     public static void main(String[] args) {
+        System.out.println("\n----------------------------------------------------------");
+        System.out.println("Массив вещественных чисел: ");
         printMass(arrFloat);
-        delValues(arrFloat,12, 212, 34, 34);
+        System.out.println("\nМассив вещественных чисел после удаления заданного значения: ");
+        printMass(removeNumbers(arrFloat, 2434.22));
+        System.out.println("\n----------------------------------------------------------");
+        System.out.println("Элементы:");
+        printMassInt(arrInt);
+        System.out.println("\nМаксимум среди отрицательных элементов: " + getMaxNumLessZero(arrInt));
+
+        System.out.println("----------------------------------------------------------");
+        String s = "Министерство внутренних дел Российской Федерации";
+        System.out.println("Текст: " + s);
+        System.out.println("Аббревиатура из текста: " + getAbbreviation(s));
+        System.out.println("----------------------------------------------------------");
+    }
+    public static void printMass(double array[]){
+        String doArrayString = Arrays.toString(array);
+        System.out.print(doArrayString);
+    }
+    public static void printMassInt(int arrayI[]){
+        String intArrayString = Arrays.toString(arrayI);
+        System.out.print(intArrayString);
     }
 
-    public static void printMass(float array[]){
-        String intArrayString = Arrays.toString(array);
-        System.out.print("\n"+intArrayString);
-    }
-
-
-    public static void delValues(float[] array, float ...nums){
-
-        List<?> list = Arrays.asList(array);
-
-        System.out.println(" ");
-        for(int i = 0;i < array.length;i++){
-            for(int e = 0;e < nums.length;e++){
-                if (array[i] == nums[e]){
-                    list.remove(array[i]);
-                }
+    // Метод, удаляющий из массива вещественных чисел заданные значения
+    public static double[] removeNumbers(double[] array, double num ) {
+        List<Double> list = new ArrayList<>();
+        for (double n : array) {
+            if (n != num) {
+                list.add(n);
             }
         }
+        double[] result = new double[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i).doubleValue();
+        }
+        return result;
     }
 
+    // Метод, вычисляющий максимум среди элементов, имеющих отрицательное значение.
+    public static int getMaxNumLessZero(int[] inputArray){
+        int maxValue = inputArray[0];
+        List<Integer> listLessZero = new ArrayList<>();
+        for (int n : inputArray) {
+            if (n < 0) {
+                listLessZero.add(n);
+            }
+        }
+        int[] arrayLessZero = new int[listLessZero.size()];
+        for(int i = 1; i < arrayLessZero.length; i++){
+            if(inputArray[i] > maxValue){
+            maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
 
-
-
+    // Метод, получающий аббревиатуру заданного текста
+    public static String getAbbreviation(String str){
+        String result = str.replaceAll("\\B.|\\P{L}", "").toUpperCase();
+        return result;
+    }
 }
